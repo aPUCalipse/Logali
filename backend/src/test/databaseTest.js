02ff4138c9a6c15a7c6e680334c86fcdb960d01f
Luiz Henrique Silva Jesus <luizhenriquesilvajesus@gmail.com>
2020-03-12 18:11:50 -0300

    corrigindo nomes das variavies

--- backend/src/test/databaseTest.js
+++ backend/src/test/databaseTest.js
@@ -1,11 +1,11 @@
-const MysqlResvice = require('../dataBase/MySQLService')
+const MysqlService = require('../dataBase/MySQLService')
 const chai = require('chai')
 const assert = chai.assert
 
-async function testConnection(){
+async function testConnection() {
     try {
-        const con = await MysqlResvice.getConnection()
-    } catch(e){
+        const con = await MysqlService.getPool()
+    } catch (e) {
         console.log(e)
         assert.fail()
     }
