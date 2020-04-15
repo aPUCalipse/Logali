e19fe66ba53ba5a39525dd28963bf388dc99d08d
Ariel Barcelos <arioptimizer@gmail.com>
2020-04-15 01:01:48 -0300

    Alterações na homepage

--- frontend/src/Views/HomePage/HomePage.js
+++ frontend/src/Views/HomePage/HomePage.js
@@ -1,14 +1,13 @@
 //react e bootstrap
 import React from 'react'
-import {Container, Row, Col} from 'react-bootstrap'
+import {Row, Col} from 'react-bootstrap'
 
 //componentes 
 import Header from '../../Components/Header/Header'
 import Footer from '../../Components/Footer/Footer'
-import Background from '../../Components/Background/Background'
 
 //Imagens
-import mountain from '../../Images/mountain.jpg'
+import location from '../../Images/location.svg'
 
 //stylesheet
 import style from './HomePage.module.css'
@@ -22,24 +21,16 @@ cep('05010000')
 function HomePage(){
     return(
         <div className={style.HomePage_Container}>
-            <Container>
+            <Header />
                 <Row>
-                    <Col md={12}>
-                        <Header />
-                        <Background 
-                            className={style.Background_Image}
-                            image={mountain} 
-                            description="Man carrying boxes"
-                            height="100%"
-                            width="100%"
-                            spotlight="Du. Du hast. Du Hast Mich."
-                            // common_text="Hier Kommt Die Sonne, Eins Zwei Drei"
-                        />
-                        {/* <Button variant="light">Light</Button> <Button variant="dark">Dark</Button>{' '} */}
-                        <Footer />
+                    <Col md={6}>
+                        <img className={style.Image_location_svg} src={location}></img>
+                    </Col>
+                    <Col md={6}>
+                        <h2 className={style.Main_text_spotlight}>Faça seus agendamentos de serviços de forma fácil e rápida! É só clicar Logo Ali!</h2>
                     </Col>
                 </Row>
-            </Container>
+            <Footer /> 
         </div>
     )
 }
