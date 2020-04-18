7f424ff81cfffc9e2d7c8f17f6fe860d3f28abcd
Luiz Henrnique SIlva Jesus <luizhenriquesilvajesus@gmail.com>
2020-04-18 02:21:01 -0300

    correções na tela de cadastro

--- frontend/src/Components/Address/Address.js
+++ frontend/src/Components/Address/Address.js
@@ -9,35 +9,35 @@ function Address() {
                         <label htmlFor="state">Estado: </label>
                     </div>
                     <div className="form-group col-md-3">
-                        <select name="estado" placeholder="Estado" id="inputEstado" value={this.state.estadoOption} onChange={this.handleEstadoChange}>
-                            <option value="" disabled defaultValue hidden>Estado</option>
-                            <option value="AC" >Acre</option>
-                            <option value="AL">Alagoas</option>
-                            <option value="AM">Amazonas</option>
-                            <option value="AP">Amap·</option>
-                            <option value="BA">Bahia</option>
-                            <option value="CE">Cear·</option>
-                            <option value="DF">Distrito Federal</option>
-                            <option value="ES">EspÌrito Santo</option>
-                            <option value="GO">Goi·s</option>
-                            <option value="MA">Maranh„o</option>
-                            <option value="MT">Mato Grosso</option>
-                            <option value="MS">Mato Grosso do Sul</option>
-                            <option value="MG">Minas Gerais</option>
-                            <option value="PA">Par·</option>
-                            <option value="PB">ParaÌba</option>
-                            <option value="PR">Paran·</option>
-                            <option value="PE">Pernambuco</option>
-                            <option value="PI">PiauÌ</option>
-                            <option value="RJ">Rio de Janeiro</option>
-                            <option value="RN">Rio Grande do Norte</option>
-                            <option value="RO">RondÙnia</option>
-                            <option value="RS">Rio Grande do Sul</option>
-                            <option value="RR">Roraima</option>
-                            <option value="SC">Santa Catarina</option>
-                            <option value="SE">Sergipe</option>
-                            <option value="SP">S„o Paulo</option>
-                            <option value="TO">Tocantins</option>
+                        <select name="estado" placeholder="Estado">
+                            <option name="optionStates" value="">Selecione o seu estado por favor</option>
+                            <option name="optionStates" value="AC" >Acre</option>
+                            <option name="optionStates" value="AL">Alagoas</option>
+                            <option name="optionStates" value="AM">Amazonas</option>
+                            <option name="optionStates" value="AP">Amap√°</option>
+                            <option name="optionStates" value="BA">Bahia</option>
+                            <option name="optionStates" value="CE">Cear√°</option>
+                            <option name="optionStates" value="DF">Distrito Federal</option>
+                            <option name="optionStates" value="ES">Esp√≠rito Santo</option>
+                            <option name="optionStates" value="GO">Goi√°s</option>
+                            <option name="optionStates" value="MA">Maranh√£o</option>
+                            <option name="optionStates" value="MT">Mato Grosso</option>
+                            <option name="optionStates" value="MS">Mato Grosso do Sul</option>
+                            <option name="optionStates" value="MG">Minas Gerais</option>
+                            <option name="optionStates" value="PA">Par√°</option>
+                            <option name="optionStates" value="PB">Para√≠ba</option>
+                            <option name="optionStates" value="PR">Paran√°</option>
+                            <option name="optionStates" value="PE">Pernambuco</option>
+                            <option name="optionStates" value="PI">Piau√≠</option>
+                            <option name="optionStates" value="RJ">Rio de Janeiro</option>
+                            <option name="optionStates" value="RN">Rio Grande do Norte</option>
+                            <option name="optionStates" value="RO">Rond√¥nia</option>
+                            <option name="optionStates" value="RS">Rio Grande do Sul</option>
+                            <option name="optionStates" value="RR">Roraima</option>
+                            <option name="optionStates" value="SC">Santa Catarina</option>
+                            <option name="optionStates" value="SE">Sergipe</option>
+                            <option name="optionStates" value="SP">S√£o Paulo</option>
+                            <option name="optionStates" value="TO">Tocantins</option>
                         </select>
                     </div>
                 </div>
@@ -56,7 +56,7 @@ function Address() {
 
                 <div className="form-row">
                     <div className="form-group col-md-4">
-                        <label htmlFor="cep">CÛdigo Postal: </label>
+                        <label htmlFor="cep">C√≥digo Postal: </label>
                         <input type="text" name="cep" className="form-control" id="cepInput" placeholder="Digite seu CEP" autoComplete="off" />
                     </div>
                     <div className="form-group col-md-4">
@@ -64,12 +64,12 @@ function Address() {
                         <input type="text" name="street" className="form-control" id="ruaInput" placeholder="Digite sua rua" autoComplete="off" />
                     </div>
                     <div className="form-group col-md-4">
-                        <label htmlFor="name">N˙mero: </label>
-                        <input type="number" name="name" className="form-control" id="numberInput" placeholder="Digite o n˙mero da casa" autoComplete="off" />
+                        <label htmlFor="name">N√∫mero: </label>
+                        <input type="number" name="name" className="form-control" id="numberInput" placeholder="Digite o nÔøΩmero da casa" autoComplete="off" />
                     </div>
                     <div className="form-group col-md-4">
                         <label htmlFor="complemento">Complemento: </label>
-                        <input type="text" name="name" className="form-control" id="complementoInput" placeholder="H· complemento?" autoComplete="off" />
+                        <input type="text" name="name" className="form-control" id="complementoInput" placeholder="HÔøΩ complemento?" autoComplete="off" />
                     </div>
                 </div>
             </div>
--- frontend/src/Views/RegisterPage/Register.js
+++ frontend/src/Views/RegisterPage/Register.js
@@ -12,11 +12,6 @@ class Register extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {
-            selectedOption: "2",
-            estadoOption: 'Estado'
-        };
-
     }
 
     hideAddress() {
@@ -33,23 +28,45 @@ class Register extends Component {
         });
     }
 
-    handleEstadoChange = (event) => {
-        let self = this;
-        self.setState({
-            estadoOption: event.target.value
-        });
-    }
-
     handleRegister = async () => {
-        if ((document.getElementById('senhaInput').value === document.getElementById('confirmaInput').value)) {
-            if (document.getElementById('clienteRadio').checked || document.getElementById('prestadorRadio').checked) {
-                const response = await api.post('/logali/app/register/create', {
-                    nome: document.getElementById('nameInput').value, senha: document.getElementById('senhaInput').value, login: document.getElementById('userInput').value,
-                    tipoUsuario: this.state.selectedOption, rua: document.getElementById('ruaInput').value, numero: document.getElementById('numberInput').value,
-                    bairro: document.getElementById('bairroInput').value, cidade: document.getElementById('cidadeInput').value, estado: this.state.estadoOption,
-                    cep: document.getElementById('cepInput').value, complemento: document.getElementById('complementoInput').value, geolocX: -19.932661, geolocY: - 43.936254
+        if (
+            (document.getElementById('senhaInput').value === document.getElementById('confirmaInput').value)
+        ) {
+            if (
+                    document.getElementById('clienteRadio').checked ||
+                    document.getElementById('prestadorRadio').checked
+            ) {
+                const optionsState = document.getElementsByName('optionStates')
+
+                console.log(optionsState)
+                console.log(optionsState.length)
+
+                let stateSelected = null
+
+                for (let i = 0; i < optionsState.length; i++) {
+                    if(optionsState[i].selected){
+                        stateSelected = optionsState[i].value
+                        break
+                    }
+                }
+
+                const response = await api.post(
+                    '/logali/app/register/create',
+                    {
+                        nome: document.getElementById('nameInput').value,
+                        senha: document.getElementById('senhaInput').value,
+                        login: document.getElementById('userInput').value,
+                        tipoUsuario: this.state.selectedOption,
+                        rua: document.getElementById('ruaInput').value,
+                        numero: document.getElementById('numberInput').value,
+                        bairro: document.getElementById('bairroInput').value,
+                        cidade: document.getElementById('cidadeInput').value,
+                        estado: stateSelected,
+                        cep: document.getElementById('cepInput').value, 
+                        complemento: document.getElementById('complementoInput').value, 
+                        geolocX: -19.932661,
+                        geolocY: -43.936254
                 });
-                //          console.log(await response.json());
                 if (response.status !== 200) {
                     throw Error(response.body.message);
 
@@ -71,7 +88,7 @@ class Register extends Component {
 
     render() {
         return (
-            <>
+            <div>
                 <Header />
                 <div className={style.Margin}>
                     <div className={style.Register}>
@@ -114,83 +131,13 @@ class Register extends Component {
                                     </div>
                                 </div>
                             </div>
-                            { /*<Address /> */}
-                            <div id="enderecoCliente" style={{ display: 'none' }}>
-                                <div className="form-row">
-                                    <div className="form-group col-md-2">
-                                        <label htmlFor="state">Estado: </label>
-                                    </div>
-                                    <div className="form-group col-md-3">
-                                        <select name="estado" placeholder="Estado" id="inputEstado" value={this.state.estadoOption} onChange={this.handleEstadoChange}>
-                                            <option value="" disabled defaultValue hidden>Estado</option>
-                                            <option value="AC" >Acre</option>
-                                            <option value="AL">Alagoas</option>
-                                            <option value="AM">Amazonas</option>
-                                            <option value="AP">Amapá</option>
-                                            <option value="BA">Bahia</option>
-                                            <option value="CE">Ceará</option>
-                                            <option value="DF">Distrito Federal</option>
-                                            <option value="ES">Espírito Santo</option>
-                                            <option value="GO">Goiás</option>
-                                            <option value="MA">Maranhão</option>
-                                            <option value="MT">Mato Grosso</option>
-                                            <option value="MS">Mato Grosso do Sul</option>
-                                            <option value="MG">Minas Gerais</option>
-                                            <option value="PA">Pará</option>
-                                            <option value="PB">Paraíba</option>
-                                            <option value="PR">Paraná</option>
-                                            <option value="PE">Pernambuco</option>
-                                            <option value="PI">Piauí</option>
-                                            <option value="RJ">Rio de Janeiro</option>
-                                            <option value="RN">Rio Grande do Norte</option>
-                                            <option value="RO">Rondônia</option>
-                                            <option value="RS">Rio Grande do Sul</option>
-                                            <option value="RR">Roraima</option>
-                                            <option value="SC">Santa Catarina</option>
-                                            <option value="SE">Sergipe</option>
-                                            <option value="SP">São Paulo</option>
-                                            <option value="TO">Tocantins</option>
-                                        </select>
-                                    </div>
-                                </div>
-
-                                <div className="form-row">
-                                    <div className="form-group col-md-6">
-                                        <label htmlFor="city">Cidade: </label>
-                                        <input type="text" name="city" className="form-control" id="cidadeInput" placeholder="Digite sua cidade" autoComplete="off" />
-                                    </div>
-
-                                    <div className="form-group col-md-6">
-                                        <label htmlFor="bairro">Bairro: </label>
-                                        <input type="text" name="bairro" className="form-control" id="bairroInput" placeholder="Digite seu bairro" autoComplete="off" />
-                                    </div>
-                                </div>
-
-                                <div className="form-row">
-                                    <div className="form-group col-md-4">
-                                        <label htmlFor="cep">Código Postal: </label>
-                                        <input type="text" name="cep" className="form-control" id="cepInput" placeholder="Digite seu CEP" autoComplete="off" />
-                                    </div>
-                                    <div className="form-group col-md-4">
-                                        <label htmlFor="street">Rua: </label>
-                                        <input type="text" name="street" className="form-control" id="ruaInput" placeholder="Digite sua rua" autoComplete="off" />
-                                    </div>
-                                    <div className="form-group col-md-4">
-                                        <label htmlFor="name">Número: </label>
-                                        <input type="number" name="name" className="form-control" id="numberInput" placeholder="Digite o número da casa" autoComplete="off" />
-                                    </div>
-                                    <div className="form-group col-md-4">
-                                        <label htmlFor="complemento">Complemento: </label>
-                                        <input type="text" name="name" className="form-control" id="complementoInput" placeholder="Há complemento?" autoComplete="off" />
-                                    </div>
-                                </div>
-                            </div>
+                            <Address />
                         </form>
                         <button className="btn btn-success" onClick={this.handleRegister}>Registre-se</button>
                     </div>
                 </div>
                 <Footer />
-            </>
+            </div>
         );
     }
 }
