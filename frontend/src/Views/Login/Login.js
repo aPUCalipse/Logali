609db4dea17f2a9b043c0c9eed18cf0f9f5b6a12
Luiz Henrnique SIlva Jesus <luizhenriquesilvajesus@gmail.com>
2020-04-22 01:21:49 -0300

    pagina de login com a integração

--- frontend/src/Views/Login/Login.js
+++ frontend/src/Views/Login/Login.js
@@ -1,107 +1,164 @@
-import React, { Component } from 'react';
+import { ToastProvider, useToasts } from 'react-toast-notifications'
+import React, { useState, Component } from 'react';
 import {navigate, A} from 'hookrouter';
-
-import {Row, Col, Form, Button} from 'react-bootstrap'
-
+import {Row, Col, Form, Button, Container} from 'react-bootstrap'
 import api from '../../Components/Assets/api';
 
-class Login extends Component {
+import style from './Login.module.css'
 
-    constructor(props){
-        super(props)
-        this.state = {
-            login: "",
-            password: "",
-            typeUser: ""
-        }
+const FormLogin = () => {
+    const { addToast } = useToasts()
 
-        this.handleInputChange = this.handleInputChange.bind(this)
-        this.login = this.login.bind(this)
-    }
+    const [loginData, setInputValues] = useState({
+        login: "",
+        password: "",
+        typeUser: ""
+    });
+      
+    const handleInputChange = event => {
+        const { id, value } = event.target;
+        setInputValues({ ...loginData, [id]: value });
+    };
 
-    handleInputChange(event){
-        const id = event.target.id
-        const value = event.target.value
-
-        this.setState({
-            [id]: value
-        })
-    }
-
-    async login(){
+    const login = async () =>{
         try{
-            if(!this.state.login || this.state.login.length < 1){
-                alert("O login deve ser preenchido")
+            if(!loginData.login || loginData.login.length < 1){
+                addToast(
+                    'O login deve ser preenchido', 
+                    { 
+                        appearance: 'error',
+                        autoDismiss: true
+                    }
+                )
                 return
             }
 
-            if(!this.state.password){
-                alert("A senha deve ser preenchida")
+            if(!loginData.password){
+                addToast(
+                    'A senha deve ser preenchida', 
+                    { 
+                        appearance: 'error',
+                        autoDismiss: true
+                    }
+                )
                 return 
-            } else if(this.state.password.length < 8){
-                alert("A senha deve conter no minimo 8 carateres")
+            } else if(loginData.password.length < 8){
+                addToast(
+                    'A senha deve conter no minimo 8 carateres', 
+                    { 
+                        appearance: 'error',
+                        autoDismiss: true
+                    }
+                )
                 return 
             }
 
-            if(!this.state.typeUser || this.state.typeUser.length < 1){
-                alert("O tipo de usuario deve ser preenchido")
+            if(!loginData.typeUser || loginData.typeUser.length < 1){
+                addToast(
+                    'O tipo de usuario deve ser preenchido', 
+                    { 
+                        appearance: 'error',
+                        autoDismiss: true
+                    }
+                )
                 return
-            }
+             }
 
-            const requestLogin = await api.post("/logali/app/scheduling/select", this.state)
+            await api.post("/logali/app/scheduling/select", loginData)
 
-            if(this.state.typeUser === '1'){
-                navigate("/agendar")
-            } else if(this.state.typeUser === '2') {
-                navigate("/agendamentos")
-            }
+            addToast(
+                'Tudo certo, você será redirecionado...', 
+                { 
+                    appearance: 'success',
+                    autoDismiss: true,
+                    onDismiss: () => {
+                        if(loginData.typeUser === '1'){
+                            navigate("/agendar")
+                        } else if(loginData.typeUser === '2') {
+                            navigate("/agendamentos")
+                        }
+                    }
+                }
+            )
         } catch (err) {
             console.log("EROORR")
             console.log(err)
 
 
             if(err && err.response && err.response.data && err.response.data.message){
-                alert(err.response.data.message)
+                addToast(
+                    'rr.response.data.message', 
+                    { 
+                        appearance: 'error',
+                        autoDismiss: true
+                    }
+                )
             } else {
-                alert(`Algo Inesperado Ocorreu: \n ${err}`)
+                addToast(
+                    `Algo Inesperado Ocorreu: \n ${err}`, 
+                    { 
+                        appearance: 'error',
+                        autoDismiss: true
+                    }
+                )
             }
         }
     }
 
-    render() {
+    return (
+        <div className={style.backgound}>
+            <Container>
+                <div className={style.greenBox}>
+                    <div className={style.boxLogin}>
+                        <Form as={Row}>
+                            <Form.Group as={Col} md={12}>
+                                <Form.Label>Login:</Form.Label>
+                                <Form.Control type="text" placeholder="Digite o login" id="login" onChange={handleInputChange}/>
+                            </Form.Group>
+
+                            <Form.Group as={Col} md={12}>
+                                <Form.Label>Senha:</Form.Label>
+                                <Form.Control type="password" placeholder="Digite seu nome" id="password" onChange={handleInputChange}/>
+                            </Form.Group>
+
+                            <Form.Group as={Col} md={12} name="typeUser">
+                                <Form.Label>Tipo de usuario</Form.Label>
+                                <Form.Control as="select"custom id="typeUser" onChange={handleInputChange}>
+                                    <option name="optionTypeUser" value="">Selecione o tipo de usuario</option>
+                                    <option name="optionTypeUser" value="1">Técnico</option>
+                                    <option name="optionTypeUser" value="2">Cliente</option>
+                                </Form.Control>
+                            </Form.Group>
+
+                            <Col md={12}>
+                                <A href="/register  ">Não tem uma conta? Junte-se a Nós</A>
+                            </Col>
+
+                            <Col md={12}>
+                                <Button variant="success" onClick={login}>Logar</Button>
+                            </Col>
+                        </Form>
+                    </div>
+                </div>
+            </Container>
+        </div>
+    )
+}
+
+class Login extends Component{
+    constructor(props){
+        super(props)
+    }
+
+    render(){
         return (
-            <div>
-                <Form as={Row}>
-                    <Form.Group as={Col} md={12}>
-                        <Form.Label>Login:</Form.Label>
-                        <Form.Control type="text" placeholder="Digite o login" id="login" value={this.state.login} onChange={this.handleInputChange}/>
-                    </Form.Group>
-
-                    <Form.Group as={Col} md={12}>
-                        <Form.Label>Senha:</Form.Label>
-                        <Form.Control type="password" placeholder="Digite seu nome" id="password" value={this.state.password} onChange={this.handleInputChange}/>
-                    </Form.Group>
-
-                    <Form.Group as={Col} md={12} name="typeUser">
-                        <Form.Label>Tipo de usuario</Form.Label>
-                        <Form.Control as="select"custom id="typeUser" value={this.state.typeUser} onChange={this.handleInputChange}>
-                            <option name="optionTypeUser" value="">Selecione como deseja logar</option>
-                            <option name="optionTypeUser" value="1">Técnico</option>
-                            <option name="optionTypeUser" value="2">Cliente</option>
-                        </Form.Control>
-                    </Form.Group>
-
-                    <Col md={12}>
-                        <A href="/register">Não tem uma conta? Junte-se a Nós</A>
-                    </Col>
-
-                    <Col md={12}>
-                        <Button variant="success" onClick={this.login}>Logar</Button>
-                    </Col>
-                </Form>
-            </div>
-        );
+            <ToastProvider
+                autoDismissTimeout={3500}
+            >
+                <FormLogin />
+            </ToastProvider>
+        )
     }
 }
 
-export default Login;
+export default Login
