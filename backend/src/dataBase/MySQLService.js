3cfb467aa60ce0335cc33fb110995b197d4385cc
Luiz Henrnique SIlva Jesus <luizhenriquesilvajesus@gmail.com>
2020-05-15 17:01:00 -0300

    mais correções de merge de código

--- backend/src/dataBase/MySQLService.js
+++ backend/src/dataBase/MySQLService.js
@@ -2,13 +2,13 @@ const MYSQL = require("mysql");
 const util = require("util");
 
 function getPool() {
-    const connection = MYSQL.createPool({
-        connectionLimit: 10,
-        host: 'localhost',
-        user: 'root',
-        password: 'admin',
-        database: 'logali'
-    });
+  const connection = MYSQL.createPool({
+    connectionLimit: 10,
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'logali'
+  });
 
   connection.query = util.promisify(connection.query).bind(connection);
   if (connection) {
